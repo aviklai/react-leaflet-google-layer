c60209c832cac3999ee4ea8c8f71ec2c7ff82744
Avi <aviklaiman@hotmail.com>
2019-03-08 08:28:43 +0200

    Added google mutant types as a dependency

--- src/index.ts
+++ src/index.ts
@@ -4,75 +4,7 @@ import { ContextProps, GridLayer, withLeaflet } from 'react-leaflet';
 import 'leaflet.gridlayer.googlemutant';
 import { TileLayerOptions, Point } from 'leaflet';
 
-// Type definitions for leaflet.gridlayer.googlemutant 0.4
-// Project: https://gitlab.com/IvanSanchez/Leaflet.GridLayer.GoogleMutant#README
-// Definitions by: Ernest Rhinozeros <https://github.com/ernest-rhinozeros>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.3
-type GoogleMutantType = 'roadmap' | 'satellite' | 'terrain' | 'hybrid';
-
-interface IGoogleMutantStyler {
-  hue?: string;
-  lightness?: number;
-  saturation?: number;
-  gamma?: number;
-  invert_lightness?: boolean;
-  visibility?: string;
-  color?: string;
-  weight?: number;
-}
-
-/**
- * Google's map style.
- *
- * https://developers.google.com/maps/documentation/javascript/style-reference
- */
-interface IGoogleMutantStyle {
-  /**
-   * https://developers.google.com/maps/documentation/javascript/style-reference#style-features
-   */
-  featureType?: string;
-
-  /**
-   * https://developers.google.com/maps/documentation/javascript/style-reference#style-elements
-   */
-  elementType?: string;
-
-  /**
-   * https://developers.google.com/maps/documentation/javascript/style-reference#stylers
-   */
-  stylers?: IGoogleMutantStyler[];
-}
-
-interface IGoogleMutantOptions extends TileLayerOptions {
-  minZoom?: number;
-  maxZoom?: number;
-  maxNativeZoom?: number;
-  tileSize?: number | Point;
-  subdomains?: string | string[];
-  errorTileUrl?: string;
-
-  /**
-   * The mutant container will add its own attribution anyways.
-   */
-  attribution?: string;
-
-  opacity?: number;
-  continuousWorld?: boolean;
-  noWrap?: boolean;
-
-  /**
-   * Google's map type. 'hybrid' is not really supported.
-   */
-  type?: GoogleMutantType;
-
-  /**
-   * Google's map styles.
-   */
-  styles?: IGoogleMutantStyle[];
-}
-
-interface IProps extends IGoogleMutantOptions, ContextProps {
+interface IProps extends L.gridLayer.GoogleMutantOptions, ContextProps {
   zIndex?: number;
 }
 
