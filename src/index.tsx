73489a927380be0d6a71b8f2d69b37d2bd9a576a
Avi <aviklaiman@hotmail.com>
2019-03-08 11:02:05 +0200

    Integrating google-maps loader

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,19 @@ import * as React from 'react';
 import * as L from 'leaflet';
 import { ContextProps, GridLayer, withLeaflet } from 'react-leaflet';
 import 'leaflet.gridlayer.googlemutant';
+import * as GoogleMapsLoader from 'google-maps';
 
+type GoogleMapsLoaderTypes = typeof GoogleMapsLoader;
 interface IProps extends L.gridLayer.GoogleMutantOptions, ContextProps {
   zIndex?: number;
+  googleMapsLoaderConf: GoogleMapsLoaderTypes
 }
 
 class ReactLeafletGoogleLayer extends GridLayer<IProps> {
   public createLeafletElement(props: IProps) {
+    let googleMapsLoader = GoogleMapsLoader;
+    googleMapsLoader = Object.assign(googleMapsLoader, props.googleMapsLoaderConf);
+    GoogleMapsLoader.load();
     this.leafletElement = L.gridLayer.googleMutant(props);
     return this.leafletElement;
   }
