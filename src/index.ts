931a673000d5de31e308b1974d420ad4d1af29d2
Avi <aviklaiman@hotmail.com>
2019-03-08 18:37:15 +0200

    Version release

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,27 @@
 import * as React from 'react';
 import * as L from 'leaflet';
 import { ContextProps, GridLayer, withLeaflet } from 'react-leaflet';
-import { gridLayer } from 'leaflet';
 import 'leaflet.gridlayer.googlemutant';
+import * as GoogleMapsLoader from 'google-maps';
 
-interface IProps extends gridLayer.GoogleMutantOptions, ContextProps {
-  opacity?: number;
+interface IProps extends L.gridLayer.GoogleMutantOptions, ContextProps {
   zIndex?: number;
+  useGoogMapsLoader?: boolean;
+  googleMapsLoaderConf?: Partial<typeof GoogleMapsLoader>;
 }
 
 class ReactLeafletGoogleLayer extends GridLayer<IProps> {
+  public static defaultProps: IProps = {
+    useGoogMapsLoader: true,
+    googleMapsLoaderConf: { VERSION: undefined },
+  };
+
   public createLeafletElement(props: IProps) {
+    if (props.useGoogMapsLoader) {
+      let googleMapsLoader = GoogleMapsLoader;
+      googleMapsLoader = Object.assign(googleMapsLoader, props.googleMapsLoaderConf);
+      GoogleMapsLoader.load();
+    }
     this.leafletElement = L.gridLayer.googleMutant(props);
     return this.leafletElement;
   }
