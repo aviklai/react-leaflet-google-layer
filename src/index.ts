728958728f7a6be55b8dc50d23be6cafa34c6720
Avi <aviklaiman@hotmail.com>
2020-10-13 20:37:34 +0300

    Added google layers

--- src/index.ts
+++ src/index.ts
@@ -4,27 +4,49 @@ import { ContextProps, GridLayer, withLeaflet } from 'react-leaflet';
 import 'leaflet.gridlayer.googlemutant';
 import * as GoogleMapsLoader from 'google-maps';
 
+
+interface IGoogleMapsAddLayer {
+  name: 'BicyclingLayer' | 'TrafficLayer' | 'TransitLayer';
+  options?: any;
+}
+
 interface IProps extends L.gridLayer.GoogleMutantOptions, ContextProps {
   zIndex?: number;
   useGoogMapsLoader?: boolean;
   googleMapsLoaderConf?: Partial<typeof GoogleMapsLoader>;
+  googleMapsAddLayers?: IGoogleMapsAddLayer[];
+  ref?: React.Ref<any>;
 }
 
 class ReactLeafletGoogleLayer extends GridLayer<IProps> {
   public static defaultProps: IProps = {
     useGoogMapsLoader: true,
     googleMapsLoaderConf: { VERSION: undefined },
+    googleMapsAddLayers: undefined
   };
 
   public createLeafletElement(props: IProps) {
-    const { useGoogMapsLoader, googleMapsLoaderConf, leaflet, ...googleMutantProps } = props;
+    const { useGoogMapsLoader, googleMapsLoaderConf, googleMapsAddLayers, leaflet, ...googleMutantProps } = props;
     if (useGoogMapsLoader) {
       let googleMapsLoader = GoogleMapsLoader;
       googleMapsLoader = Object.assign(googleMapsLoader, googleMapsLoaderConf);
       googleMapsLoader.load();
     }
-    this.leafletElement = L.gridLayer.googleMutant(googleMutantProps);
+    this.leafletElement = L.gridLayer.googleMutant(googleMutantProps)
+    if (googleMapsAddLayers) {
+      googleMapsAddLayers.forEach((layer) => {
+        (this.leafletElement as L.gridLayer.GoogleMutant).addGoogleLayer(layer.name, layer.options);
+      });
+    }    
     return this.leafletElement;
+  }
+
+  public addGoogleLayer = (name: string, options?: any) => {
+    (this.leafletElement as L.gridLayer.GoogleMutant).addGoogleLayer(name, options);
+  }
+
+  public removeGoogleLayer = (name: string) => {
+    (this.leafletElement as L.gridLayer.GoogleMutant).removeGoogleLayer(name);
   }
 
   public updateLeafletElement(prevProps: IProps, nextProps: IProps) {
