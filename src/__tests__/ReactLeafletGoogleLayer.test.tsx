852835208ec115091192c315cad2d6c4d63bb4c2
yuiseki <yuiseki@gmail.com>
2021-05-12 16:27:42 +0900

    update test

--- src/__tests__/ReactLeafletGoogleLayer.test.tsx
+++ src/__tests__/ReactLeafletGoogleLayer.test.tsx
@@ -3,18 +3,22 @@ import ReactDOM from 'react-dom';
 import ReactLeafletGoogleLayer from '../index';
 import { MapContainer } from 'react-leaflet';
 import * as ReactTestUtils from 'react-dom/test-utils';
+import { initialize } from '@googlemaps/jest-mocks';
 
-
-test('ReactLeafletGoogleLayer', () => {
-  expect(ReactLeafletGoogleLayer);   
-
-  const dom = ReactTestUtils.renderIntoDocument(
-    <div>
-      <MapContainer>
-        <ReactLeafletGoogleLayer />
-      </MapContainer>
-    </div>
-  ) as any;
-  const component = ReactDOM.findDOMNode(dom.childNodes[0]) as any;
-  expect(component).toBeInstanceOf(HTMLElement);
+describe('ReactLeafletGoogleLayer', () => {
+  beforeEach(() => {
+    initialize();
+  })
+  it('Draw HTML element', () => {
+    expect(ReactLeafletGoogleLayer);   
+    const dom = ReactTestUtils.renderIntoDocument(
+      <div>
+        <MapContainer>
+          <ReactLeafletGoogleLayer />
+        </MapContainer>
+      </div>
+    ) as any;
+    const component = ReactDOM.findDOMNode(dom.childNodes[0]) as any;
+    expect(component).toBeInstanceOf(HTMLElement);
+  })
 });
